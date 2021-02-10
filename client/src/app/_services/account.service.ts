55a6385e4fe607a20a4560025bbd7b51e0980cb4
Jobin George <jobinkbg@gmail.com>
2021-02-10 17:56:07 +0530

    rooting

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, mapTo } from 'rxjs/operators';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -11,18 +11,16 @@ export class AccountService {
   baseUrl = "https://localhost:5001/api/";
   private currentUserSource = new ReplaySubject<any>(1);
   currentUser$ = this.currentUserSource.asObservable();
-  loginUser = "user";
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
-      map((res: any) => {
-        const user: User = res;
+      mapTo((res: User) => {
+        const user = res;
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
-          this.loginUser = user.username;
         }
       })
     )
@@ -30,12 +28,11 @@ export class AccountService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
-      map((res: any) => {
+      mapTo((res: User) => {
         const user: User = res;
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
-          this.loginUser = user.username;
         }
       })
     )
@@ -43,12 +40,10 @@ export class AccountService {
 
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
-    this.loginUser = user.username;
   }
 
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
-    this.loginUser = "user";
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
@@ -11,23 +13,21 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public accountService: AccountService) {
-    this.model.loginUser = this.accountService.loginUser;
-   }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(responce => {
-        console.log(responce);
-        this.model.loginUser = this.accountService.loginUser;
+        this.router.navigateByUrl('/members');
       }, error => {
-        console.log(error);
+        this.toastr.error(error.error);
       });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
